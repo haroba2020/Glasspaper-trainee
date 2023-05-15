0f7f9d0e973e7d0f5c3755464b35726b59fdd5f8
Fredrik <fredriktheyerdahl@gmail.com>
2023-05-15 22:23:12 +0200

    adding search bar function

--- src/Main.js
+++ src/Main.js
@@ -1,30 +1,42 @@
-import { useState, useEffect, useRef } from "react"
+import React, { useState, useEffect } from "react";
 import Navbar from "./Navbar";
 import Card from "./Card";
-import persons from "./persons"
-const Main = () => {
-    const [value, setValue] = useState('');
+import persons from "./persons";
 
-    const handleValueChange = (newValue) => {
-      setValue(newValue);
-      console.log(value + ' new value')
-    };
-    return (
-        <div className="main">
-            <Navbar onValueChange={handleValueChange} />
-            <div className="card-wrapper">
-                {persons.map((person) => (
-                    <Card
-                    key={person.id} // Use the unique id as the key prop
-                    name={person.name}
-                    age={person.age}
-                    address={person.address}
-                    hobbies={person.hobbies}
-                    />
-                ))}
-            </div>
-        </div>
-      );
-}
- 
+const Main = () => {
+  const [value, setValue] = useState("");
+  const [filteredPersons, setFilteredPersons] = useState(persons);
+
+  useEffect(() => {
+    console.log(value + " new value");
+
+    // Filter persons based on the input value
+    const filtered = persons.filter((person) =>
+      person.name.toLowerCase().includes(value.toLowerCase())
+    );
+    setFilteredPersons(filtered);
+  }, [value]);
+
+  const handleValueChange = (newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className="main">
+      <Navbar onValueChange={handleValueChange} />
+      <div className="card-wrapper">
+        {filteredPersons.map((person) => (
+          <Card
+            key={person.id}
+            name={person.name}
+            age={person.age}
+            address={person.address}
+            hobbies={person.hobbies}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
 export default Main;
